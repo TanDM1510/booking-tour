30d3b2d8d9765f5b76c78bbffb65ec0f4df13e49
Duong Minh Tan (K16 HCM) <tandmss160653@fpt.edu.vn>
2024-03-14 08:12:38 +0700

    update

---
+++ src/components/dashboard/DashboardStatsGrid.jsx
@@ -0,0 +1,134 @@
+import { useEffect, useState } from "react";
+import { IoBagHandle, IoPieChart, IoPeople, IoCart } from "react-icons/io5";
+import { getAllBookings } from "../../redux/features/bookings/bookings";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllUser } from "../../redux/features/user/allUser";
+import { getAllTrips } from "../../redux/features/trips/trips";
+
+export default function DashboardStatsGrid() {
+  const { bookings } = useSelector((store) => store.booking);
+  console.log(bookings);
+  let totalAmount = 0;
+
+  if (bookings && bookings.length > 0) {
+    totalAmount = bookings.reduce((accumulator, currentBooking) => {
+      return accumulator + currentBooking.totalAmount;
+    }, 0);
+  }
+  const { users } = useSelector((store) => store.allUser);
+  const { trips } = useSelector((store) => store.trips);
+  const [tourguide, setTourGuide] = useState("");
+  console.log(trips);
+
+  useEffect(() => {
+    if (trips && trips.length > 0) {
+      // Tạo một đối tượng để lưu trữ số lần xuất hiện của mỗi tourguideId
+      const tourguideIdCount = {};
+
+      // Lặp qua mảng trips và đếm số lần xuất hiện của mỗi tourguideId
+      trips.forEach((trip) => {
+        const { tourGuideId } = trip;
+        if (tourguideIdCount[tourGuideId]) {
+          tourguideIdCount[tourGuideId]++;
+        } else {
+          tourguideIdCount[tourGuideId] = 1;
+        }
+      });
+
+      // Tìm tourguideId xuất hiện nhiều nhất
+      let mostFrequentTourguideId;
+      let maxCount = 0;
+      Object.keys(tourguideIdCount).forEach((tourGuideId) => {
+        if (tourguideIdCount[tourGuideId] > maxCount) {
+          mostFrequentTourguideId = tourGuideId;
+          maxCount = tourguideIdCount[tourGuideId];
+        }
+      });
+
+      console.log(
+        "Tourguide ID xuất hiện nhiều nhất:",
+        mostFrequentTourguideId
+      );
+      setTourGuide(mostFrequentTourguideId);
+    } else {
+      console.log("Không có dữ liệu trips để xử lý.");
+    }
+  }, [trips]);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getAllTrips());
+    dispatch(getAllBookings());
+    dispatch(getAllUser());
+  }, []);
+  return (
+    <div className="flex gap-4">
+      <BoxWrapper>
+        <div className="rounded-full h-12 w-12 flex items-center justify-center bg-sky-500">
+          <IoBagHandle className="text-2xl text-white" />
+        </div>
+        <div className="pl-4">
+          <span className="text-sm text-gray-500 font-light">Total Sales</span>
+          <div className="flex items-center">
+            <strong className="text-xl text-gray-700 font-semibold">
+              {totalAmount} VND
+            </strong>
+          </div>
+        </div>
+      </BoxWrapper>
+      <BoxWrapper>
+        <div className="rounded-full h-12 w-12 flex items-center justify-center bg-orange-600">
+          <IoPieChart className="text-2xl text-white" />
+        </div>
+        <div className="pl-4">
+          <span className="text-sm text-gray-500 font-light">
+            Best Tour Guide
+          </span>
+          <div className="flex items-center">
+            <strong className="text-xl text-gray-700 font-semibold">
+              {tourguide}
+            </strong>
+          </div>
+        </div>
+      </BoxWrapper>
+      <BoxWrapper>
+        <div className="rounded-full h-12 w-12 flex items-center justify-center bg-yellow-400">
+          <IoPeople className="text-2xl text-white" />
+        </div>
+        <div className="pl-4">
+          <span className="text-sm text-gray-500 font-light">
+            Total Customers
+          </span>
+          <div className="flex items-center">
+            <strong className="text-xl text-gray-700 font-semibold">
+              {users.length}
+            </strong>
+          </div>
+        </div>
+      </BoxWrapper>
+      <BoxWrapper>
+        <div className="rounded-full h-12 w-12 flex items-center justify-center bg-green-600">
+          <IoCart className="text-2xl text-white" />
+        </div>
+        <div className="pl-4">
+          <span className="text-sm text-gray-500 font-light">
+            Total Bookings
+          </span>
+          <div className="flex items-center">
+            <strong className="text-xl text-gray-700 font-semibold">
+              {bookings.length}
+            </strong>
+          </div>
+        </div>
+      </BoxWrapper>
+    </div>
+  );
+}
+
+function BoxWrapper({ children }) {
+  return (
+    <div className="bg-white rounded-sm p-4 flex-1 border border-gray-200 flex items-center">
+      {children}
+    </div>
+  );
+}
--- src/pages/dashboard/Stats.jsx
+++ src/pages/dashboard/Stats.jsx
@@ -1,4 +1,20 @@
+import BuyerProfilePieChart from "../../components/dashboard/BuyerProfilePieChart";
+import DashboardStatsGrid from "../../components/dashboard/DashboardStatsGrid";
+import TransactionChart from "../../components/dashboard/TransactionChart";
+
 const Stats = () => {
-  return <div>Stats</div>;
+  return (
+    <div className="flex flex-col gap-4">
+      <DashboardStatsGrid />
+      <div className="flex flex-row gap-4 w-full">
+        <TransactionChart />
+        <BuyerProfilePieChart />
+      </div>
+      <div className="flex flex-row gap-4 w-full">
+        {/* <RecentOrders />
+        <PopularProducts /> */}
+      </div>
+    </div>
+  );
 };
 export default Stats;
